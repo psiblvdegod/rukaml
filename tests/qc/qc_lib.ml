ea51268edd1cde3566d7275721ded3752c478904
Kakadu <Kakadu@pm.me>
2024-11-13 16:49:08 +0300

    Remove manual generator
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- tests/qc/qc_lib.ml
+++ tests/qc/qc_lib.ml
@@ -43,63 +43,6 @@ type expr =
       string := (string [@gen varname]);
       list := (list [@gen short_list (gen_expr_sized (n / 3))])])]
 [@@deriving qcheck]
-(* include struct
-  let _ = fun (_ : expr) -> ()
-
-  let rec gen_expr_sized n =
-    match n with
-    | 0 ->
-      QCheck.Gen.frequency
-        [ 1, QCheck.Gen.pure EUnit
-        ; 1, QCheck.Gen.map (fun gen0 -> EConst gen0) gen_const
-        ; 1, QCheck.Gen.map (fun gen0 -> EVar gen0) varname
-        ]
-    | _ ->
-      QCheck.Gen.frequency
-        [ 1, QCheck.Gen.pure EUnit
-        ; 1, QCheck.Gen.map (fun gen0 -> EConst gen0) gen_const
-        ; 1, QCheck.Gen.map (fun gen0 -> EVar gen0) varname
-        ; ( 1
-          , QCheck.Gen.map
-              (fun (gen0, gen1, gen2) -> EIf (gen0, gen1, gen2))
-              (QCheck.Gen.triple
-                 (gen_expr_sized (n / 2))
-                 (gen_expr_sized (n / 2))
-                 (gen_expr_sized (n / 2))) )
-        ; ( 1
-          , QCheck.Gen.map
-              (fun (gen0, gen1) -> ELam (gen0, gen1))
-              (QCheck.Gen.pair gen_pattern (gen_expr_sized (n / 2))) )
-        ; ( 1
-          , QCheck.Gen.map
-              (fun (gen0, gen1) -> EApp (gen0, gen1))
-              (QCheck.Gen.pair (gen_expr_sized (n / 2)) (gen_expr_sized (n / 2))) )
-        ; ( 1
-          , QCheck.Gen.map
-              (fun (gen0, gen1, gen2) -> ETuple (gen0, gen1, gen2))
-              (QCheck.Gen.triple
-                 (gen_expr_sized (n / 2))
-                 (gen_expr_sized (n / 2))
-                 (short_list (gen_expr_sized (n / 2)))) )
-        ; ( 1
-          , QCheck.Gen.map
-              (fun (gen0, gen1, gen2, gen3) -> ELet (gen0, gen1, gen2, gen3))
-              (QCheck.Gen.quad
-                 gen_rec_flag
-                 gen_pattern
-                 (gen_expr_sized (n / 2))
-                 (gen_expr_sized (n / 2))) )
-        ]
-  ;;
-
-  let _ = gen_expr_sized
-  let gen_expr = QCheck.Gen.sized gen_expr_sized
-  let _ = gen_expr
-  let arb_expr_sized n = QCheck.make @@ gen_expr_sized n
-  let _ = arb_expr_sized
-  let arb_expr = QCheck.make @@ gen_expr
-  let _ = arb_expr
-end *)
 
 let arbitrary_expr =
   let open QCheck.Iter in
