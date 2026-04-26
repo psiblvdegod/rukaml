cb1b1827031581e444ed52498e8b3d982233bf4d
prettysorrow <artyombaly@list.ru>
2026-04-27 00:57:39 +0300

    update tests/compiler/program.cpp
    Signed-off-by: prettysorrow <artyombaly@list.ru>

--- tests/compiler/program.cpp
+++ tests/compiler/program.cpp
@@ -1,9 +1,20 @@
+int five()
+{
+    int x = 3;
+    x = x + 2;
+    return x;
+}
+
+int seven()
+{
+    int x = 4;
+    int z = x + 1;
+    return z + 2;
+}
+
 int main()
 {
-    int x = 5;
-    int y = 10;
-    int z = 20;
-
-    return x + y + z;
+    int f = five();
+    int s = seven();
+    return f + s;
 }
-\
